a845dd8b9c16ea2590eb2c438d32e2d8707c4590
Tahaxweb <tancilingir@gmail.com>
2024-04-10 02:33:26 +0300

    add banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+type BannerProps = {};
+
+const Banner = (props: BannerProps) => {
+  return (
+    <div className="w-full h-12  gap-x-4 items-center flex justify-center text-white bg-salesforce-950">
+      <p className="font-medium">Say hello to Einstein Copilot. </p>
+      <Link href={"/"} className=" text-white border-b border-white ">
+        Find out more
+      </Link>
+    </div>
+  );
+};
+
+export { Banner };
