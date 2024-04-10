3263ffbfdde83060df8d1c9eef79c8e9cd90b946
Tahaxweb <tancilingir@gmail.com>
2024-04-11 00:32:36 +0300

    add button variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,11 +10,11 @@ type ButtonProps = {
 const Button = ({ variant = "primary", onClick, children }: ButtonProps) => {
   return (
     <button
-      className={clsx(" rounded-md py-2.5 px-6", {
+      className={clsx(" rounded-md py-3 px-6", {
         " bg-salesforce-600 hover:bg-salesforce-950   text-white":
           variant === "primary",
         "  bg-green-700 hover:bg-green-950   text-white": variant === "green",
-        " border-2 border-salesforce-600 text-salesforce-700  hover:bg-salesforce-300   hover:bg-opacity-20":
+        " border-2 border-salesforce-600 hover:border-salesforce-950 text-salesforce-700  hover:bg-salesforce-300   hover:bg-opacity-20":
           variant === "secondary",
       })}
       onClick={onClick}
