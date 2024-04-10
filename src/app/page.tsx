8c0b52ece2dbb2ad167fdd42eb79c120a45435e3
Tahaxweb <tancilingir@gmail.com>
2024-04-10 03:04:38 +0300

    add banner components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Banner } from "@/components/Banner";
 
 import Header from "@/components/Header";
+import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <Hero />
     </>
   );
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,7 @@ type BannerProps = {};
 
 const Banner = (props: BannerProps) => {
   return (
-    <div className="w-full h-12  gap-x-4 items-center flex justify-center text-white bg-salesforce-950">
+    <div className="w-full h-12  gap-x-4 items-center flex-wrap flex justify-center text-white bg-salesforce-950">
       <p className="font-medium">Say hello to Einstein Copilot. </p>
       <Link href={"/"} className=" text-white border-b border-white ">
         Find out more
